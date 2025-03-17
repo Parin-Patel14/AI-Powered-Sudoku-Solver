62d69f2d8c2ac58d48e90e71be0887d7065283ad
Parin Patel <148354003+Parin-Patel14@users.noreply.github.com>
2025-03-17 19:59:16 -0400

    Added BFS algorithm and performance tracking files

---
+++ algorithms/BFS/metrics_performance.py
@@ -0,0 +1,32 @@
+import re
+
+def count_data(filename, data_to_count):
+    total_count = 0
+    count_occurrences = 0
+    with open(filename, 'r') as file:
+        for line in file:
+            if data_to_count in line:
+                match = re.search(r'\d+\.*\d*', line)
+                if match:
+                    count = float(match.group())
+                    total_count += count
+                    count_occurrences += 1
+    return total_count, count_occurrences
+
+def calculate_avg(filename, data_to_count):
+    total, occurrences = count_data(filename, data_to_count)
+    if occurrences != 0:
+        avg = total / occurrences
+    else:
+        avg = 0
+    return avg
+
+filename = 'sudoku_solver_output.txt'  
+data_to_count = ['Total states processed', 'Time taken to solve'] 
+
+for data in data_to_count:
+    avg = calculate_avg(filename, data)
+    with open("metrics_performance.txt", "a") as file:
+        file.write(f"Average {data}: {avg}\n")
+    print(f"Average {data}: {avg}")
+
